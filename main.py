1a6920968f611a398dcf4de70b4ce1dd8fe0f890
metulburr <micahpage911@yahoo.com>
2014-04-22 17:22:55 -0400

    added kv config

--- main.py
+++ main.py
@@ -7,28 +7,15 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 
-class Application(App):
+class ScatterTextWidget(BoxLayout):
+    pass
+
+class MainApp(App):
     def build(self):
-        default = 'monkey'
-        b = BoxLayout(orientation='vertical')
-        t = TextInput(font_size=150,
-                      size_hint_y=None,
-                      height=200,
-                      text=default)
-
-        f = FloatLayout()
-        s = Scatter()
-        l = Label(text=default,
-                  font_size=150)
-
-        t.bind(text=l.setter('text'))
-
-        f.add_widget(s)
-        s.add_widget(l)
-
-        b.add_widget(t)
-        b.add_widget(f)
-        return b
+        return ScatterTextWidget()
 
 if __name__ == "__main__":
-    Application().run()
+    MainApp().run()
+
+
+
